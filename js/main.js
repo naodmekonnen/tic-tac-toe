eef314cc41f31533c01cbb9b17c0ec0353d0e738
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-10-07 04:06:06 +0000

    3

--- js/main.js
+++ js/main.js
@@ -1,155 +1,63 @@
-const containerDiv = document.getElementById('main')
-containerDiv.setAttribute('class', 'container text-center')
-
-function setLayout(parent, tag, text, className, idName, number) {
-    let element = document.createElement(tag)
-    element.innerText = text
-    if (className) {
-        element.setAttribute('class', className)
-    };
-    if (idName) {
-        element.setAttribute('id', idName)
-    }
-    if (number){
-        element.setAttribute('data-tile', number)
-    }
-    parent.appendChild(element)
-};
-
-function setBoard(){
-    setLayout(containerDiv,'div','','','gameContainer')
-
-    setLayout(gameContainer, 'div', '', 'row', 'topRow')
-    setLayout(topRow, 'button', '', 'col btn', 'col1', '0')
-    setLayout(topRow, 'button', '', 'col btn', 'col2', '1')
-    setLayout(topRow, 'button', '', 'col btn', 'col3', '2')
-    setLayout(gameContainer, 'div', '', 'row', 'middleRow')
-    setLayout(middleRow, 'button', '', 'col btn', 'col4', '3')
-    setLayout(middleRow, 'button', '', 'col btn', 'col5', '4')
-    setLayout(middleRow, 'button', '', 'col btn', 'col6', '5')
-    setLayout(gameContainer, 'div', '', 'row', 'bottomRow')
-    setLayout(bottomRow, 'button', '', 'col btn', 'col7', '6')
-    setLayout(bottomRow, 'button', '', 'col btn', 'col8', '7')
-    setLayout(bottomRow, 'button', '', 'col btn', 'col9', '8')
-
-};
-setBoard()
 
 
 
-// const winConditions = [
-//         ['0','1','2'],
-//         ['3','4','5'],
-//         ['6','7','8'],
-//         ['0','3','6'],
-//         ['1','4','7'],
-//         ['2','5','8'],
-//         ['0','4','8'],
-//         ['2','4','6']
-// ];
+    function generateElements(parentEl, tag, text, className, idName, cellIndexName, styleName) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let boardState = ['','','','','','','','',''];
-// let currentPlayer = 'X';
-// let btn = document.querySelectorAll('col btn');
-
-// let displayDiv = document.getElementById('main');
-
-// let gameInfo = document.createElement('h2');
-//     gameInfo.textContent = '';
-//     gameInfo.id = 'infoID';
-
-// let resetButton = document.createElement('button');
-//     resetButton.textContent = 'Reset Game';
-//     resetButton.className = 'reset';
-//     resetButton.id = 'buttonID';
-
-// displayDiv.append(gameInfo,resetButton);
-
-// let showStatus = document.getElementById('infoID')
-// let gameStatus = true;
-
-// let win = () => `Player ${currentPlayer} wins`;
-// let draw = () => `It's a tie`;
-// let playerturn = () => `It's ${currentPlayer}'s turn`;
-
-// showStatus.innerHTML = playerturn;
-
-// function playerMove(clickedTile, tileIndex){
-//     boardState[tileIndex] = currentPlayer;
-//     clickedtile.innerHTML = currentPlayer;
-// }
-
-// function switchPlayer(){
-//     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-//     showStatus.innerHTML = playerturn();
-// }
-
-// function checkWin(){
-//     let winGame = false;
-//     for(let i=0; i<=8; i++){
-//         let condition = winConditions[i];
-//         let A = boardState[condition[0]];
-//         let B = boardState[condition[1]];
-//         let C = boardState[condition[2]];
+        let element = document.generateElements(tag)                     
+        element.innerText = text                                    
+        if (className) {                                             
+            element.setAttribute('class', className);                
+        }
     
-//     if(A === '' || B === ''|| C == '')
-//         continue;
-//     if(A === B  && B === C){
-//         winGame = true;
-//         break;
-//     }
-// }
-// switchPlayer()
-// }
-
-// function tileClicked(clickedbox){
-//     let clickedTile = clickedbox.target;
-//     let tileIndex = clickedTile.getAttribute('data-tile');
-
-//     if(boardState[tileIndex] !== '' || !gameStatus)
-//     return;
-
-//     playerMove();
-//     checkWin();
-
-// }
-
-
-// let box = 'col btn';
-// console.log(box)
-// function resetGame(){
-// gameStatus = true;
-// currentPlayer = 'X';
-// let boardState = ['','','','','','','','',''];
-// showStatus.innerHTML = playerturn();
-// document.querySelectorAll('box').forEach(box => box.innerHTML = '');
-
-// }
-
-// document.querySelectorAll('box')
-// .
+        if (idName) {                                                
+            element.setAttribute('id', idName);                      
+        }
+    
+        if (cellIndexName) {
+            element.setAttribute('cellIndex', cellIndexName);
+        }
+    
+        if (styleName) {
+            element.setAttribute('style', styleName);
+        }
+    
+    
+        parentEl.appendChild(element)                               
+    }
+    
+    const mainContainer = document.getElementById('main')            
+    
+    
+    function setBoard() {
+    
+        generateElements(mainContainer, 'h1', 'Tic Tac Toe', '', '', '', '',);
+        generateElements(mainContainer, 'div', '', 'card', 'mainCard', '', '',);
+        generateElements(mainCard, 'div', '', 'row', 'rowOne', '', 'height: 147px',);
+        generateElements(rowOne, 'div', '', 'col', 'columnOneA', '', '',);
+        generateElements(columnOneA, 'div', '', 'cell', '', '0', 'height: 120px',);
+        generateElements(rowOne, 'div', '', 'col', 'columnOneB', '', '',);
+        generateElements(columnOneB, 'div', '', 'cell', '', '1', 'height: 120px',);
+        generateElements(rowOne, 'div', '', 'col', 'columnOneC', '', '',);
+        generateElements(columnOneC, 'div', '', 'cell', '', '2', 'height: 120px',);
+    
+        generateElements(mainCard, 'div', '', 'row', 'rowTwo', '', 'height: 147px',);
+        generateElements(rowTwo, 'div', '', 'col', 'columnTwoA', '', '',);
+        generateElements(columnTwoA, 'div', '', 'cell', '', '3', 'height: 120px',);
+        generateElements(rowTwo, 'div', '', 'col', 'columnTwoB', '', '',);
+        generateElements(columnTwoB, 'div', '', 'cell', '', '4', 'height: 120px',);
+        generateElements(rowTwo, 'div', '', 'col', 'columnTwoC', '', '',);
+        generateElements(columnTwoC, 'div', '', 'cell', '', '5', 'height: 120px',);
+    
+        generateElements(mainCard, 'div', '', 'row', 'rowThree', '', 'height: 147px',);
+        generateElements(rowThree, 'div', '', 'col', 'columnThreeA', '', '',);
+        generateElements(columnThreeA, 'div', '', 'cell', '', '6', 'height: 120px',);
+        generateElements(rowThree, 'div', '', 'col', 'columnThreeB', '', '',);
+        generateElements(columnThreeB, 'div', '', 'cell', '', '7', 'height: 120px',);
+        generateElements(rowThree, 'div', '', 'col', 'columnThreeC', '', '',);
+        generateElements(columnThreeC, 'div', '', 'cell', '', '8', 'height: 120px',);
+    
+        generateElements(mainContainer, 'h2', '', '', 'statusText', '', '',);
+        generateElements(mainContainer, 'button', 'Restart', 'btn btn-outline-info btn-lg', 'restartBtn', '', '',);
+    }
+    setBoard();
+    
