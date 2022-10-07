4a0c3578a2af3d664aedeab69824f6e7f5d95884
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-10-07 03:24:20 +0000

    simpler code

--- js/main.js
+++ js/main.js
@@ -37,57 +37,119 @@ setBoard()
 
 
 
-const winConditions = [
-        ['0','1','2'],
-        ['3','4','5'],
-        ['6','7','8'],
-        ['0','3','6'],
-        ['1','4','7'],
-        ['2','5','8'],
-        ['0','4','8'],
-        ['2','4','6']
-];
+// const winConditions = [
+//         ['0','1','2'],
+//         ['3','4','5'],
+//         ['6','7','8'],
+//         ['0','3','6'],
+//         ['1','4','7'],
+//         ['2','5','8'],
+//         ['0','4','8'],
+//         ['2','4','6']
+// ];
 
-let boardState = ['','','','','','','','',''];
-let currentPlayer = 'X';
-let btn = document.querySelectorAll('col btn');
 
-let displayDiv = document.getElementById('main');
 
-let gameInfo = document.createElement('h2');
-    gameInfo.textContent = '';
-    gameInfo.id = 'infoID';
 
-let resetButton = document.createElement('button');
-    resetButton.textContent = 'Reset Game';
-    resetButton.className = 'reset';
-    resetButton.id = 'buttonID';
 
-displayDiv.append(gameInfo,resetButton);
 
-let showStatus = document.getElementById('infoID')
-let gameStatus = true;
 
-let win = () => `Player ${currentPlayer} wins`;
-let draw = () => `It's a tie`;
-let playerturn = () => `It's ${currentPlayer}'s turn`;
 
-showStatus.innerHTML = playerturn;
 
-function tileClicked(clickedtile, tileIndex){
-    boardState[tileIndex] = currentPlayer;
-    clickedtile.innerHTML = currentPlayer;
-}
 
-function switchPlayer(){
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    showStatus.innerHTML = playerturn();
-}
 
-function checkWin(){
-    let winGame = false;
-    for(let i=0; i<=8; i++){
-        let condition = winConditions[i];
-        let A = 
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let boardState = ['','','','','','','','',''];
+// let currentPlayer = 'X';
+// let btn = document.querySelectorAll('col btn');
+
+// let displayDiv = document.getElementById('main');
+
+// let gameInfo = document.createElement('h2');
+//     gameInfo.textContent = '';
+//     gameInfo.id = 'infoID';
+
+// let resetButton = document.createElement('button');
+//     resetButton.textContent = 'Reset Game';
+//     resetButton.className = 'reset';
+//     resetButton.id = 'buttonID';
+
+// displayDiv.append(gameInfo,resetButton);
+
+// let showStatus = document.getElementById('infoID')
+// let gameStatus = true;
+
+// let win = () => `Player ${currentPlayer} wins`;
+// let draw = () => `It's a tie`;
+// let playerturn = () => `It's ${currentPlayer}'s turn`;
+
+// showStatus.innerHTML = playerturn;
+
+// function playerMove(clickedTile, tileIndex){
+//     boardState[tileIndex] = currentPlayer;
+//     clickedtile.innerHTML = currentPlayer;
+// }
+
+// function switchPlayer(){
+//     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+//     showStatus.innerHTML = playerturn();
+// }
+
+// function checkWin(){
+//     let winGame = false;
+//     for(let i=0; i<=8; i++){
+//         let condition = winConditions[i];
+//         let A = boardState[condition[0]];
+//         let B = boardState[condition[1]];
+//         let C = boardState[condition[2]];
+    
+//     if(A === '' || B === ''|| C == '')
+//         continue;
+//     if(A === B  && B === C){
+//         winGame = true;
+//         break;
+//     }
+// }
+// switchPlayer()
+// }
+
+// function tileClicked(clickedbox){
+//     let clickedTile = clickedbox.target;
+//     let tileIndex = clickedTile.getAttribute('data-tile');
+
+//     if(boardState[tileIndex] !== '' || !gameStatus)
+//     return;
+
+//     playerMove();
+//     checkWin();
+
+// }
+
+
+// let box = 'col btn';
+// console.log(box)
+// function resetGame(){
+// gameStatus = true;
+// currentPlayer = 'X';
+// let boardState = ['','','','','','','','',''];
+// showStatus.innerHTML = playerturn();
+// document.querySelectorAll('box').forEach(box => box.innerHTML = '');
+
+// }
+
+// document.querySelectorAll('box')
+// .
