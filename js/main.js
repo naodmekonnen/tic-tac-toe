de9714f7822819d0ac08affa2d3c845325a39b11
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-10-07 00:31:14 +0000

    need to fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const containerDiv = document.getElementById('main')
 containerDiv.setAttribute('class', 'container text-center')
 
-function createLayout(parent, tag, text, className, idName, number) {
+function setLayout(parent, tag, text, className, idName, number) {
     let element = document.createElement(tag)
     element.innerText = text
     if (className) {
@@ -11,31 +11,132 @@ function createLayout(parent, tag, text, className, idName, number) {
         element.setAttribute('id', idName)
     }
     if (number){
-        element.setAttribute('data-index', number)
+        element.setAttribute('data-tile', number)
     }
     parent.appendChild(element)
 };
 
 function setBoard(){
-    createLayout(containerDiv,'div','','','gameContainer')
+    setLayout(containerDiv,'div','','','gameContainer')
 
-    createLayout(gameContainer, 'div', '', 'row', 'topRow')
-    createLayout(topRow, 'button', '', 'col btn', 'col1', '0')
-    createLayout(topRow, 'button', '', 'col btn', 'col2', '1')
-    createLayout(topRow, 'button', '', 'col btn', 'col3', '2')
-    createLayout(gameContainer, 'div', '', 'row', 'middleRow')
-    createLayout(middleRow, 'button', '', 'col btn', 'col4', '3')
-    createLayout(middleRow, 'button', '', 'col btn', 'col5', '4')
-    createLayout(middleRow, 'button', '', 'col btn', 'col6', '5')
-    createLayout(gameContainer, 'div', '', 'row', 'bottomRow')
-    createLayout(bottomRow, 'button', '', 'col btn', 'col7', '6')
-    createLayout(bottomRow, 'button', '', 'col btn', 'col8', '7')
-    createLayout(bottomRow, 'button', '', 'col btn', 'col9', '8')
+    setLayout(gameContainer, 'div', '', 'row', 'topRow')
+    setLayout(topRow, 'button', '', 'col btn', 'col1', '0')
+    setLayout(topRow, 'button', '', 'col btn', 'col2', '1')
+    setLayout(topRow, 'button', '', 'col btn', 'col3', '2')
+    setLayout(gameContainer, 'div', '', 'row', 'middleRow')
+    setLayout(middleRow, 'button', '', 'col btn', 'col4', '3')
+    setLayout(middleRow, 'button', '', 'col btn', 'col5', '4')
+    setLayout(middleRow, 'button', '', 'col btn', 'col6', '5')
+    setLayout(gameContainer, 'div', '', 'row', 'bottomRow')
+    setLayout(bottomRow, 'button', '', 'col btn', 'col7', '6')
+    setLayout(bottomRow, 'button', '', 'col btn', 'col8', '7')
+    setLayout(bottomRow, 'button', '', 'col btn', 'col9', '8')
 
 };
 setBoard()
-console.log(setBoard)
+// console.log(setBoard)
 
+let displayDiv = document.getElementById('main');
+
+let gameInfo = document.createElement('h2');
+    gameInfo.textContent = '';
+    gameInfo.id = 'infoID';
+
+let resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset Game';
+    resetButton.id = 'buttonID';
+
+displayDiv.append(gameInfo,resetButton);
+
+const tiles = document.querySelectorAll('.col btn');
+const statusText = document.querySelectorAll('#infoID');
+
+const winConditions = [
+        ['0','1','2'],
+        ['3','4','5'],
+        ['6','7','8'],
+        ['0','3','6'],
+        ['1','4','7'],
+        ['2','5','8'],
+        ['0','4','8'],
+        ['2','4','6']
+];
+
+let box = 'col btn'
+let boardState = ['','','','','','','','',''];
+let currentPlayer = 'X';
+let gameOn = false;
+
+function init(){
+    tiles.forEach(box => box.addEventListener('click', tileClicked));
+    resetButton.addEventListener('click', resetGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    gameOn = true;
+    console.log(resetButton)
+}
+
+function tileClicked(){
+    const tileIndex = this.getAttribute('data-tile');
+    if(boardState[tileIndex] != '' || !gameOn){
+        return;
+    } 
+    updateTile(this, tileIndex);
+    checkWin();
+}
+
+function updateTile(box, index){
+    boardState[index] = currentPlayer;
+    box.textContent = currentPlayer;
+    console.log(currentPlayer)
+}
+
+
+function switchPlayer(){
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWin(){
+    let game = false;
+    for(let i=0; i<winConditions.length; i++){
+        let condition = winConditions[i];
+        
+        let tileA = boardState[condition[0]];
+        let tileB = boardState[condition[1]];
+        let tileC = boardState[condition[2]];
+
+        if(tileA == '' || tileB == '' || tileC == ''){
+            continue;
+        }
+        if(tileA == tileB && tileB == tileC){
+            game = true;
+            break;
+        }
+    
+    }
+        if(game){
+            statusText.textContent = `${currentPlayer} wins!!`;
+            gameOn = false;
+        }
+        else if(boardState.includes('')){
+            statusText.textContent = "It's a tie";
+            gameOn = false;
+        }
+        else{
+            switchPlayer();
+        }
+
+}
+
+function resetGame(){
+    currentPlayer = 'X';
+    let boardState = ['','','','','','','','',''];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    tiles.forEach(box => box.textContent = '');
+    gameOn = true;
+
+
+}
 
 
 
@@ -83,34 +184,12 @@ console.log(setBoard)
 //         createDiv();
 //     }
 
-// let displayDiv = document.getElementById('main');
 
-// let gameInfo = document.createElement('h2');
-//     gameInfo.textContent = '';
-//     gameInfo.id = 'infoID';
-
-// let resetButton = document.createElement('button');
-//     resetButton.textContent = 'Reset Game';
-//     resetButton.id = 'buttonID';
-
-// displayDiv.append(gameInfo,resetButton);
 
 // // let cells = document.querySelectorAll('.boxes');
 
-// let winConditions = [
-//         ['0','1','2'],
-//         ['3','4','5'],
-//         ['6','7','8'],
-//         ['0','3','6'],
-//         ['1','4','7'],
-//         ['2','5','8'],
-//         ['0','4','8'],
-//         ['2','4','6']
-//     ];
 
-// let boardState = ['','','','','','','','',''];
-// let currentPlayer = 'X';
-// let gameOn = true;
+
 // let tiles = Array.from(document.querySelector('.boxes'));
 // console.log(tiles);
 
